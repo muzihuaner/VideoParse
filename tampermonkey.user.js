5dd51b9aac4302ac95fb608f32194a3a25f7ceca
muzihuaner <muzihuaner@hotmail.com>
2024-06-30 22:46:54 +0800

    修改

--- tampermonkey.user.js
+++ tampermonkey.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
-// @name Whoami视频解析
-// @namespace    https://ljxnet.cn/
+// @name 快点视频解析
+// @namespace    https://quickso.cn/
 // @version      4.1.4
 // @description    在各大视频网站的播放页面按~键即可跳过广告直接播放VIP视频，无任何页面上的修改
 // @author       ljxi
@@ -30,7 +30,7 @@
 
     document.onkeydown = function(event){
       if(event.keyCode == "33" || event.keyCode == "34"|| event.keyCode == "192"){
-      window.location.href='https://v.ljxnet.cn/?url='+location.href
+      window.location.href='https://jx.quickso.cn/?url='+location.href
       };
     };
 
